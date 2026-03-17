f96a3b5cf3e53786870c87ca2724183b16a929bc
nobrien97 <nicholasobrien97@gmail.com>
2026-03-17 17:11:10 +1000

    misc comments

--- include/SharedLibrary.h
+++ include/SharedLibrary.h
@@ -11,10 +11,13 @@ class SharedLibrary
 {
 private:
     void* _handle;
+
+    /// @brief Opens a handle to a library
+    /// @returns a void* to the library
     inline void* OpenHandle(const std::string& path) 
     {
 #if _WIN32
-	void* library(LoadLibraryA(path.c_str()));
+	void* library(LoadLibraryA(path.c_str())); // HMODULE/HINSTANCE are void*
 
 	if (!library) 
 	{
@@ -23,21 +26,23 @@ private:
 
 	return library;
 #else
-    void* library = dlopen(path.c_str(), RTLD_NOW);
+    void* library = dlopen(path.c_str(), RTLD_NOW); // Both Linux and Mac use dlopen
 
 	if (library == NULL)
 	{
 		return nullptr;
 	}
-    std::cout << "Loaded library at " << path << std::endl;
+    // std::cout << "Loaded library at " << path << std::endl;
 
 	return library;
 #endif
     }
+
+    /// @brief Closes a handle to a SharedLibrary (called in destructor)
     inline void CloseHandle()
     {
 #if _WIN32
-    FreeLibrary((HMODULE)_handle);
+    FreeLibrary((HMODULE)_handle); // TODO: cast to HMODULE might not be necessary
 
 #else
     int closeError = dlclose(_handle);
@@ -47,13 +52,16 @@ private:
         throw std::runtime_error("Unable to unload library, error code " + std::to_string(closeError));
     }
 
-    std::cout << "Closed library at " << reinterpret_cast<size_t>(_handle) << std::endl;
+    // std::cout << "Closed library at " << reinterpret_cast<size_t>(_handle) << std::endl;
 
     return; 
 #endif
     }
 
 public:
+
+    /// @brief Loads a new SharedLibrary from a filepath
+    /// @param path The filepath to the .dll (Windows) or .so (Linux/Mac) shared library
     explicit SharedLibrary(const std::string& path)
         : _handle(OpenHandle(path))
     {
@@ -64,6 +72,7 @@ public:
         }
     };
 
+    // Destructor automatically closes the handle 
     ~SharedLibrary() 
     {
         if (_handle)
